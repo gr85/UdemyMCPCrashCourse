f70cc120f5fced4ba96981021f77a16ebc3fec63
Guiu Riera <guiur@hotmail.com>
2026-06-19 16:50:57 +0200

    Completed vanilla mcp client with ReAct agent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from dotenv import find_dotenv, load_dotenv
+from langchain_core.messages import HumanMessage
 
 load_dotenv(find_dotenv(".env"))
 
@@ -20,7 +21,19 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello from udemymcpcrashcourse!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            # Initialize session
+            await session.initialize()
+            print("Session initialized")
+            # Get the available tools
+            tools = await load_mcp_tools(session)
+            print(tools)
+
+            agent = create_agent(model=llm, tools=tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
